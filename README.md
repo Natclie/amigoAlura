6bc1caba953e942ac175b7ddf02783f5ce76c3c1
Natclie <juanrudas2004@gmail.com>
2025-02-25 20:44:51 -0500

    +cantidad de miembros +detalles

--- README.md
+++
@@ -1,2 +0,0 @@
-# amigoscreto
-Aplicacion que permite colocar nombres en una lista, y hacer un sorteo eligiendo un ganador de todos los nombres ingresados.
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,9 @@ let inpNombre = document.getElementById('nombre');
 //BOTON SORTEAR
 let btnSortear = document.getElementById('btn-sortear');
 
+const itemsPerPage = 8;
+let currentPage = 1;
+
 function alertOn(){
     alertPop.classList.add('alertActive');
     alertPop.classList.remove('alertNotActive');
@@ -19,8 +22,6 @@ function alertOff(){
     alertPop.classList.add('alertNotActive');
     alertPop.classList.remove('alertActive');
 }
-
-
 
 function aniSortear(){
     btnSortear.addEventListener("click", function() {
@@ -33,19 +34,34 @@ function aniSortear(){
     });
 }
 
+function renderList() {
+    ulLista.innerHTML = "";
+    let start = (currentPage - 1) * itemsPerPage;
+    let end = start + itemsPerPage;
+    let paginatedItems = nombres.slice(start, end);
+    paginatedItems.forEach((nombre, index) => {
+        ulLista.innerHTML += `<li>${nombre} <button onclick="eliminarNombre(${start + index})"><i class="fa-regular fa-trash-can trash"></i></button></li>`;
+    });
+    document.getElementById("pageNumber").innerText = currentPage;
+}
+
 function agregarNombre() {
-    
     let nombre = document.getElementById("nombre").value.trim();
     if (nombre && !nombres.includes(nombre)) {
         nombres.push(nombre);
-        ulLista.innerHTML += `<li>${nombre}</li>`;
         inpNombre.value = "";
-        btnSortear.classList.add("sortear-active")
+        btnSortear.classList.add("sortear-active");
         aniSortear();
-    }else{
+        renderList();
+    } else {
         pMsg.innerText = "Agrega un nombre valido.";
         alertOn();
     }
+}
+
+function eliminarNombre(index) {
+    nombres.splice(index, 1);
+    renderList();
 }
         
 function sortear() {
@@ -63,9 +79,24 @@ function listaVacia() {
 }
 function Limpiar() {
     nombres = [];
-    ulLista.innerHTML = "";
+    currentPage = 1;
+    renderList();
     pMsg.innerText = "La lista ha sido vaciada.";
     alertOn();
+}
+
+function nextPage() {
+    if ((currentPage * itemsPerPage) < nombres.length) {
+        currentPage++;
+        renderList();
+    }
+}
+
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        renderList();
+    }
 }
 
 document.getElementById("nombre").addEventListener("keypress", function(event) {
